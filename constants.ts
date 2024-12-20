d1f1f88f1aec2206aaa9df9ed5d620e470038e46
Sinduja Ramaraj <sindujaramaraj@sindujas-mbp.lan>
2024-12-20 11:26:33 -0800

    bump version

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = "Paadam";
 export const APP_CAPTION = "Worksheet Generator";
-export const APP_VERSION = "1.0.0";
+export const APP_VERSION = "1.0.1";
 export enum ANTHROPIC_MODELS {
   CLAUDE_SONNET = "claude-3-5-sonnet-latest",
   CLAUDE_HAIKU = "claude-3-5-haiku-latest",
